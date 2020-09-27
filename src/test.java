1b322b78e57aab649ee65b1c788992fb19187ca5
kazai <kazai@qq.com>
2020-09-27 12:55:53 +0800

    add

--- src/test.java
+++ src/test.java
@@ -3,12 +3,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
-        System.out.println("helloworld");
+
     }
 
 }
