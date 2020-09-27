bc4d32cf0154a0b354940171e3e0edc991c3039c
kazai <kazai@qq.com>
2020-09-27 12:52:16 +0800

    add

--- src/test.java
+++ src/test.java
@@ -2,6 +2,13 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
     }
 
 }
