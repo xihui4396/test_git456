943079cf60fdf6fd5d9896a5b149aac09a7eebaa
xihui4396 <71883396+xihui4396@users.noreply.github.com>
2020-09-27 13:03:44 +0800

    Update and rename src/test.java to SRC/test.java

---
+++ SRC/test.java
@@ -0,0 +1,27 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+
+    }
+
+}
